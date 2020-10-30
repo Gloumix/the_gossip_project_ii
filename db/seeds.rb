71998d3c9e1435ddc2a4d85bd6d2796dc2f3e7f6
Quentin <quentin.poulain57@gmail.com>
2020-10-30 08:52:54 +0100

    last commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,17 +12,17 @@ gossips=[]
 
 require 'faker'
 
-10.times do
-  city = City.create!(name: Faker::Address.city)
-  cities << city
-end
+#10.times do
+#  city = City.create!(name: Faker::Address.city)
+#  cities << city
+#end
 
-10.times do
-  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count:5, supplemental: false, random_words_to_add: 5), email: Faker::Internet.email, age: rand(7..99), city: cities.sample)
-  users << user
-end
+#10.times do
+#  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count:5, supplemental: false, random_words_to_add: 5), email: Faker::Internet.email, age: rand(7..99), city: cities.sample)
+#  users << user
+#end
 
-20.times do
-  gossip = Gossip.create!(title: Faker::Lorem.word, content: Faker::Lorem.sentence(word_count:10, supplemental: true, random_words_to_add: 5), user: users.sample)
-  gossips << gossip
-end
+#20.times do
+#  gossip = Gossip.create!(title: Faker::Lorem.word, content: Faker::Lorem.sentence(word_count:10, supplemental: true, random_words_to_add: 5), user: users.sample)
+#  gossips << gossip
+#end
