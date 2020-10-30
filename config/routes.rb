a3c3d947a9f2d75cddc2af1ae0ce361485eb682d
Quentin <quentin.poulain57@gmail.com>
2020-10-30 06:55:40 +0100

    thirs commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
-  get '/welcome/:id', to:'welcome#show'
-  get '/contact', to:'contact#show'
-  get '/team', to:'team#show'
-  get '/profile', to:'profile#show'
+  resources :welcome, only: [:show]
+  resources :contact, only: [:index]
+  resources :team, only: [:index]
+  resources :profile, only: [:index]
   resources :gossip
   resources :user
   resources :city
